ebb230947de26e4df08277b38bd7863f2c4a3c1c
Michael <mcguiremichael1998@gmail.com>
2018-10-22 18:31:49 -0500

    Initial code

---
+++ assignment3_p1_materials/Assignment3_p1/classifier.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch import optim
+import numpy as np
+
+NUM_CLASSES = 21
+
+class Classifier(nn.Module):
+    def __init__(self, input_shape):
+        self.hidden = F.relu
+        self.input_shape = input_shape
+        super(Classifier, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 5)
+        self.conv2 = nn.Conv2d(64, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.n_size = self.conv_output(self.input_shape) 
+        
+        self.fc1 = nn.Linear(self.n_size, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, NUM_CLASSES)
+
+    def forward(self, x):
+        x = self.forward_conv(x)
+        x = x.view(x.size()[0], self.n_size)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+        
+    def forward_conv(self, x):
+        x = self.pool(self.hidden(self.conv1(x)))
+        x = self.pool(self.hidden(self.conv2(x)))
+        x = self.pool(self.hidden(self.conv3(x)))
+        return x
+        
+    def conv_output(self, shape):
+        inp = Variable(torch.rand(1, *shape))
+        out_f = self.forward_conv(inp)
+        n_size = out_f.data.view(1, -1).size(1)
+        return n_size
+
