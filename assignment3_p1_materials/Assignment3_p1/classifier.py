42ad5d726965bd042cd4a61dbe392864cfe1e120
Michael <mcguiremichael1998@gmail.com>
2018-10-30 08:41:26 -0500

    Working configuration

--- assignment3_p1_materials/Assignment3_p1/classifier.py
+++ assignment3_p1_materials/Assignment3_p1/classifier.py
@@ -12,42 +12,46 @@ class Classifier(nn.Module):
         self.hidden = F.relu
         self.input_shape = input_shape
         super(Classifier, self).__init__()
-        CONV1 = 64
-        CONV2 = 128
-        CONV3 = 128
+        CONV1 = 16
+        CONV2 = 20
+        CONV3 = 20
+        CONV4 = 20
         self.conv1 = nn.Conv2d(3, CONV1, 7, stride=2, padding=3)
         self.conv1_bn = nn.BatchNorm2d(CONV1)
         self.conv2 = nn.Conv2d(CONV1, CONV2, 5, stride=2, padding=2)
         self.conv2_bn = nn.BatchNorm2d(CONV2)
-        self.conv3 = nn.Conv2d(CONV2, CONV3, 3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(CONV2, CONV3, 4, stride=2, padding=2)
         self.conv3_bn = nn.BatchNorm2d(CONV3)
+        self.conv4 = nn.Conv2d(CONV3, CONV4, 4, stride=2, padding=2)
+        self.conv4_bn = nn.BatchNorm2d(CONV4)
         
         #self.conv4 = nn.Conv2d(32, 32, 3, padding=1)
         #self.conv5 = nn.Conv2d(32, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
         self.n_size = self.conv_output(self.input_shape) 
         
-        FC1 = 512
-        FC2 = 512
+        FC1 = 256
+        FC2 = 256
         self.fc1 = nn.Linear(self.n_size, FC1)
         self.fc1_bn = nn.BatchNorm1d(FC1)
-        self.fc2 = nn.Linear(FC1, FC2)
-        self.fc2_bn = nn.BatchNorm1d(FC2)
-        self.fc3 = nn.Linear(FC1, NUM_CLASSES)
+        #self.fc2 = nn.Linear(FC1, FC2)
+        #self.fc2_bn = nn.BatchNorm1d(FC2)
+        self.fc2 = nn.Linear(FC1, NUM_CLASSES)
         self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
         x = self.forward_conv(x)
         x = x.view(x.size()[0], self.n_size)
         x = self.dropout(self.hidden(self.fc1_bn(self.fc1(x))))
-        x = self.dropout(self.hidden(self.fc2_bn(self.fc2(x))))
-        x = self.fc3(x)
+        #x = self.dropout(self.hidden(self.fc2_bn(self.fc2(x))))
+        x = self.fc2(x)
         return x
         
     def forward_conv(self, x):
-        x = self.pool(self.hidden(self.conv1_bn(self.conv1(x))))
-        x = self.pool(self.hidden(self.conv2_bn(self.conv2(x))))
+        x = self.hidden(self.conv1_bn(self.conv1(x)))
+        x = self.hidden(self.conv2_bn(self.conv2(x)))
         x = self.hidden(self.conv3_bn(self.conv3(x)))
+        x = self.hidden(self.conv4_bn(self.conv4(x)))
         #x = self.hidden(self.conv4(x))
         #x = self.hidden(self.conv5(x))
         return x
