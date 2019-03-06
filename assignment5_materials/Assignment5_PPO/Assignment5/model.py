bda8176b0d9a7d6ea00311445c07262fd1f433ca
Michael <mcguiremichael1998@gmail.com>
2019-03-06 16:14:27 -0600

    Made model bigger

--- assignment5_materials/Assignment5_PPO/Assignment5/model.py
+++ assignment5_materials/Assignment5_PPO/Assignment5/model.py
@@ -32,8 +32,8 @@ class PPO(nn.Module):
         #self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         #self.bn3 = nn.BatchNorm2d(64)
-        self.fc = nn.Linear(9216, 256)
-        self.head = nn.Linear(256, action_size+1)
+        self.fc = nn.Linear(9216, 512)
+        self.head = nn.Linear(512, action_size+1)
         
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x))
